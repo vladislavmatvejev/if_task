623a796e0d31337147691ce7df74494650ab70cc
Vladislav Matvejev <vladislav.matvejev@elisa.ee>
2017-11-27 23:59:57 +0200

    fix

--- src/Components/ErrorMessageItem.js
+++ src/Components/ErrorMessageItem.js
@@ -4,15 +4,7 @@ import {getErrorMessage} from '../Validation/ErrorMessages';
 export default class ErrorMessageItem extends Component {
     render() {
         let errorMessage = getErrorMessage(this.props.fieldName, this.props.validationString);
-        //console.log('error messge', errorMessage);
-        //console.log('error message', this.props.validationString);
-        // if(this.props.isRequired && !this.props.isValid) {
-        //     errorMessage = ErrorMessages.isRequired(this.props.fieldName);
-        // } else if (!this.props.isRequired && !this.props.isValid) {
-        //     errorMessage = ErrorMessages.isRequired(this.props.fieldName);
-        // } else {
-        //     errorMessage = true;
-        // }
+
         return errorMessage ? [<div key={this.props.fieldName}>
             <p className={"errorMessage errorMessage-"+this.props.childClass}>
                 {errorMessage}
